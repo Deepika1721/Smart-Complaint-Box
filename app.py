7bc601f695e2ca3b69114495c48dd63d378856b5
Deepika Pandi <pandideepika@mictech.edu.in>
2026-03-27 17:22:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
-# Create database
+ADMIN_USERNAME = "Deepika"
+ADMIN_PASSWORD = "1709"
+
 def init_db():
     conn = sqlite3.connect("complaints.db")
     c = conn.cursor()
@@ -19,9 +22,25 @@ def init_db():
 
 init_db()
 
-# Home page (show complaints)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            session['admin'] = True
+            return redirect('/')
+        else:
+            return "Invalid Login"
+
+    return render_template("login.html")
+
 @app.route('/')
 def home():
+    if not session.get('admin'):
+        return redirect('/login')
+
     conn = sqlite3.connect("complaints.db")
     c = conn.cursor()
     c.execute("SELECT * FROM complaints")
@@ -29,7 +48,6 @@ def home():
     conn.close()
     return render_template("index.html", complaints=data)
 
-# Add complaint
 @app.route('/add', methods=['POST'])
 def add():
     name = request.form['name']
@@ -43,6 +61,10 @@ def add():
 
     return redirect('/')
 
-# Run app
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
